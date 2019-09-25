2c64dbb8b2b7723b4703853a8de4f8489537e5ec
Darrenni97 <Darrenni_@hotmail.com>
2019-09-25 14:23:56 -0700

    hard coded move messages (commented)

--- client.js
+++ client.js
@@ -10,9 +10,25 @@ const connect = function() {
     console.log("Successfully connected to game server");
     conn.write("Name: Bob");
   });
+
+  // conn.on('connect', () => {
+  //   setInterval(() => {
+  //     conn.write("Move: up");
+  //   }, 50);
+  //   setInterval(() => {
+  //     conn.write("Move: down");
+  //   }, 100);
+  //   setInterval(() => {
+  //     conn.write("Move: left");
+  //   }, 150);
+  //   setInterval(() => {
+  //     conn.write("Move: right");
+  //   }, 200);
+  // });
   
   conn.on('data', (data) => {
     console.log('Server says: ', data);
+    
   });
   
   conn.setEncoding('utf8');
