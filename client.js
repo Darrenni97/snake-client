01d081ad3dc4d6139ba8fd36364e280dc3008724
Darrenni97 <Darrenni_@hotmail.com>
2019-09-25 14:00:41 -0700

    event handler to print message as soon as connected

--- client.js
+++ client.js
@@ -6,6 +6,9 @@ const connect = function() {
     port: 50541
   });
   // interpret incoming data as text
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+  });
   
   conn.on('data', (data) => {
     console.log('Server says: ', data);
