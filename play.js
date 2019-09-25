d743d9ae8b79af0e2f14c6b6a68e9dcbe7884f2f
Darrenni97 <Darrenni_@hotmail.com>
2019-09-25 15:01:58 -0700

    function to handle input using stdin

--- play.js
+++ play.js
@@ -1,4 +1,19 @@
 const { connect } = require('./client');
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  const handleUserInput = (data) => {
+    if (data === '\u0003') {
+      process.exit();
+    }
+  }
+
+  stdin.on('data', handleUserInput);
+}
+
 console.log('Connecting ...');
 connect();
+setupInput();
